8714ab14bf74ab15d6f2579053bb9a452b04ce6c
Oleg Bask <obask@users.noreply.github.com>
2024-10-26 16:31:08 +0100

    Initial Commit

---
+++ TryTodoAppTests/TryTodoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TryTodoAppTests.swift
+//  TryTodoAppTests
+//
+//  Created by Oleg Bask on 10/26/24.
+//
+
+import Testing
+
+struct TryTodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
